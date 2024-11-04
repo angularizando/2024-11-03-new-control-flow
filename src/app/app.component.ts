ae4ec646090fdafbbee5386a7ee1d159dd30d677
Rafael Paiva <r.paivabr@gmail.com>
2024-11-03 21:51:47 -0300

    final example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { HttpClient } from '@angular/common/http';
+
+import { delay, map } from 'rxjs';
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'new-control-flow';
+  private readonly httpClient = inject(HttpClient);
+  todos$ = this.httpClient.get<Todo[]>(
+    'https://jsonplaceholder.typicode.com/todos'
+  );
+  todos = toSignal(this.todos$);
 }
